44376e7b3f08fb79bc3fd2827f9dbf304008b927
Kentaro Taninaka <taniken@sfc.keio.ac.jp>
2022-08-15 01:05:50 +0900

    WebGLで作ったtheta館のVRW

---
+++ Assets/MenuToPhotonManager.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuToPhotonManager : MonoBehaviourPunCallbacks
+{
+    public GameObject FirstCamera;
+    public InputField inputNickname;
+    private string selectCharacterName = "Chara1";
+
+    public void StartGame()
+    {
+        PhotonNetwork.NickName = inputNickname.text;
+        PhotonNetwork.ConnectUsingSettings();
+        FirstCamera.SetActive(false);
+    }
+
+    public void SelectChara(string CharaName)
+    {
+        selectCharacterName = CharaName;
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        var position = new Vector3(-20f, 2f, -15f);
+        PhotonNetwork.Instantiate(selectCharacterName, position, Quaternion.identity);
+        Debug.Log(selectCharacterName);
+    }
+}
---
+++ Assets/PhotonCamera.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class PhotonCamera : MonoBehaviourPunCallbacks
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!photonView.IsMine)
+        {
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
